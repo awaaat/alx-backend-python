8f3d3d63c5f2dcb2b03033a168dffe4ba1d23b9a
Allan <awaliaulah65@gmail.com>
2025-06-01 05:54:33 +0300

    renamed serializers

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path, include
 from .import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter # type: ignore
 
 router = DefaultRouter()
 router.register(r'user', views.UserViewset)
 router.register(r'conversations', views.ConversationViewSet)
 router.register(r'messages', views.MessageViewset)
 
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', views.MessageViewset, basename='conversation-messages')
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
     path('user/details/<str:first_name>/', views.UserDetailsView.as_view(), name = 'user-details'),
     path('messages/details/<uuid:message_id>/', views.MessageDetailsView.as_view(), name='message-details'),
     path('conversations/details/<uuid:conversation_id>/', views.ConversationDetailsView.as_view(), name='conversation_details'),
